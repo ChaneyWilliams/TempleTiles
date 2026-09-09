532d0c9bbcc83c18d414cc1669bb926aa31435af
ChaneyWilliams <chaneyw2004@gmail.com>
2026-09-09 11:29:07 -0500

    dynamic tiles

--- Assets/Scritps/GameManager.cs
+++ Assets/Scritps/GameManager.cs
@@ -15,6 +15,7 @@ public class GameManager : MonoBehaviour
     public Tilemap map;
 
     private Dictionary<TileBase, TileData> dataFromTile;
+    private List<Vector3Int> specialTiles = new List<Vector3Int>();
 
     private readonly List<Vector3Int> directions = new List<Vector3Int>
     {
@@ -72,6 +73,10 @@ public class GameManager : MonoBehaviour
 
             case GameState.LevelTurn:
                 yield return new WaitForSeconds(timeBetweenTurns);
+                
+                ChangeEnvironment();
+
+                yield return new WaitForSeconds(timeBetweenTurns);
                 ChangeGameState(GameState.PlayerTurn);
                 break;
         }
@@ -109,5 +114,85 @@ public class GameManager : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    public void ChangeEnvironment()
+    {
+        specialTiles.Clear();
+
+        BoundsInt bounds = map.cellBounds;
+
+        foreach (Vector3Int pos in bounds.allPositionsWithin)
+        {
+            TileData tile = GetTileFromMap(pos);
+            if (tile == null || tile.tileState == TileData.TileState.NormalTile || tile.tileState == TileData.TileState.WallTile)
+                continue;
+
+            specialTiles.Add(pos);
+        }
+
+        Dictionary<Vector3Int, TileBase> allChanges = new Dictionary<Vector3Int, TileBase>();
+
+        foreach (Vector3Int tile in specialTiles) //loop through all special tiles
+        {
+            //make a dict that has all the nighbors with the right tile type
+            //(if fire tile get earth neighbors, if earth tile get water neighbors, etc..)
+            Dictionary<Vector3Int, TileBase> changes = CheckAllNeighbors(tile); 
+
+            foreach (KeyValuePair<Vector3Int, TileBase> kvp in changes)
+            {
+                allChanges[kvp.Key] = kvp.Value; // pull all those tiles into a dict outside the loop
+            }
+        }
+        foreach (KeyValuePair<Vector3Int, TileBase> kvp in allChanges)
+        {
+            map.SetTile(kvp.Key, kvp.Value); // loop through outside dict to place new tiles without chaining new tiles (new fire tiles wont burn earth tiles etc)
+        }
+    }
+
+    Dictionary<Vector3Int, TileBase> CheckAllNeighbors(Vector3Int position)
+    {
+
+        Dictionary<Vector3Int, TileBase> changes = new Dictionary<Vector3Int, TileBase>();
+        TileData currentTile = GetTileFromMap(position);
+        if (currentTile == null)
+            return changes;
+
+        foreach (Vector3Int nextPos in GetNeighbors(position))
+        {
+            //chunky switch boi 
+            TileData neighborTile = GetTileFromMap(nextPos);
+            if (neighborTile == null)
+                continue;
+            switch ((currentTile.tileState, neighborTile.tileState))
+            {
+                case (TileData.TileState.GrassTile, TileData.TileState.WaterTile):
+                    changes[nextPos] = allTiles[2];
+                    break;
+
+                case (TileData.TileState.FireTile, TileData.TileState.GrassTile):
+                    changes[nextPos] = allTiles[1];
+                    break;
+
+                case (TileData.TileState.WaterTile, TileData.TileState.FireTile):
+                    changes[nextPos] = allTiles[2];
+                    break;
+            }
+
+        }
+
+        return changes;
+    }
+
+
+
+    List<Vector3Int> GetNeighbors(Vector3Int start)
+    {
+        List<Vector3Int> neighbors = new List<Vector3Int>();
+        foreach (Vector3Int direction in directions)
+        {
+            neighbors.Add(start + direction);
+        }
+        return neighbors;
     }
 }
